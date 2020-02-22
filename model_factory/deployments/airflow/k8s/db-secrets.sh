e7d0025a68b82a0c0b77707dd8950612a0af3e0e
David Aponte <36382674+aponte411@users.noreply.github.com>
2020-02-22 14:06:09 -0500

    Delete db-secrets.sh

--- model_factory/deployments/airflow/k8s/db-secrets.sh
+++
@@ -1,2 +0,0 @@
-kubectl create secret generic airflow-postgres --from-literal=postgres-password=$(openssl rand -base64 13)
-kubectl create secret generic airflow-redis --from-literal=redis-password=$(openssl rand -base64 13)
