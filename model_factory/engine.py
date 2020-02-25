6e25b636793ca335f744e90cb353686eb9ddcd5a
David Aponte <david.aponte@benevolent.ai>
2020-02-25 09:58:41 -0500

    WIP

--- model_factory/engine.py
+++ model_factory/engine.py
@@ -1,15 +1,39 @@
 import click.core
 
-from datasets import QuoraDataSet
-from trainers import QuoraTrainer
+from datasets import QuoraDataSet, BengaliDataSet
+from trainers import QuoraTrainer, BengaliTrainer
 
 
-def preprocess_data(path: str, fold: int) -> Tuple[pd.DataFrame]:
-    dataset = QuoraDataSet(path=path, target='is_duplicate')
-    train, val = dataset.prepare_data(fold=fold)
-    y_train, y_val = dataset.get_targets()
-    X_train, X_val = dataset.clean_data(to_drop=['is_duplicate', 'kfold'])
-    return X_train, X_val, y_train, y_val
+def run_quora_model(params: Dict, data_path: str, fold: int):
+    def preprocess_data(path: str, fold: int) -> Tuple[pd.DataFrame]:
+        dataset = QuoraDataSet(path=path, target='is_duplicate')
+        train, val = dataset.prepare_data(fold=fold)
+        y_train, y_val = dataset.get_targets()
+        X_train, X_val = dataset.clean_data(to_drop=['is_duplicate', 'kfold'])
+        return X_train, X_val, y_train, y_val
+
+    trainer = QuoraTrainer(params=params)
+    X_train, X_val, y_train, y_val = preprocess_data(path=data_path, fold=fold)
+    trainer.train_model(X_train=X_train,
+                        y_train=y_train,
+                        X_val=X_val,
+                        y_val=y_val)
+    return trainer.predict_and_score(X_new=X_val, y_new=y_val)
+
+
+def run_bengali_model(params: Dict, data_path: str, fold: int):
+    def preprocess_data(path: str, fold: int) -> Tuple[pd.DataFrame]:
+        dataset = BengaliDataSet()
+        train, val = dataset.prepare_data(fold=fold)
+        y_train, y_val = dataset.get_targets()
+        X_train, X_val = dataset.clean_data(to_drop=['is_duplicate', 'kfold'])
+        return X_train, X_val, y_train, y_val
+
+    dataset = BengaliDataSet(folds=[0, 1],
+                             image_height=137,
+                             image_width=236,
+                             mean=(0.485, 0.456, 0.406),
+                             std=(0.229, 0.224, 0.225))
 
 
 @click.command()
@@ -26,14 +50,9 @@ def runner(data_path: str, fold: int) -> pd.DataFrame:
         "n_estimators": 3000,
         "tree_method": "gpu_hist"
     }
-    trainer = QuoraTrainer(params=XGBOOST_PARAMS)
-    X_train, X_val, y_train, y_val = preprocess_data(path=data_path, fold=fold)
-    trainer.train_model(X_train=X_train,
-                        y_train=y_train,
-                        X_val=X_val,
-                        y_val=y_val)
-    preds = trainer.predict_and_score(X_new=X_val, y_new=y_val)
-    return preds
+    return run_quora_model(params=XGBOOST_PARAMS,
+                           data_path=data_path,
+                           fold=fold)
 
 
 if __name__ == "__main__":
