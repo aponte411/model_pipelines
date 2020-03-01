aa8fea1fe397e156d59da8d7813ef99d4d236f04
David Aponte <david.aponte@benevolent.ai>
2020-03-01 16:55:20 -0500

    configurable input and output paths

--- model_factory/scripts/create_image_pickles.py
+++ model_factory/scripts/create_image_pickles.py
@@ -1,13 +1,21 @@
 from datasets import BengaliDataSetTrain
-
+import click
 import utils
 
 LOGGER = utils.get_logger(__name__)
 
 
-def main():
-    utils.pickle_images(train_path="inputs/bengali_grapheme/train-folds.csv",
-                        parquet_path="inputs/bengali_grapheme/train_*.parquet")
+@click.command()
+@click.option('-in',
+              '--input',
+              type=str,
+              default="inputs/bengali_grapheme/train-folds.csv")
+@click.option('-ou',
+              '--output',
+              type=str,
+              default="inputs/bengali_grapheme/train_*.parquet")
+def main(input: str, output: str):
+    utils.pickle_images(train_path=input, parquet_path=output)
 
 
 if __name__ == "__main__":
