3b1388deb69177c30253a4b287a4d95308909977
David Aponte <david.aponte@benevolent.ai>
2020-03-10 12:06:56 -0400

    fixed Multi-GPU support

--- model_factory/engines.py
+++ model_factory/engines.py
@@ -145,6 +145,7 @@ class BengaliEngine(Engine):
         LOGGER.info(f'Using {torch.cuda.device_count()} GPUs')
         if torch.cuda.device_count() > 1:
             self.trainer.model = nn.DataParallel(self.trainer.model)
+            self.trainer.model(self.trainer.device)
         train = self._get_training_loader(folds=self.params["train_folds"],
                                           name='training')
         val = self._get_training_loader(folds=self.params["val_folds"],
@@ -333,6 +334,7 @@ class GoogleQAEngine(Engine):
         LOGGER.info(f'Using {torch.cuda.device_count()} GPUs')
         if torch.cuda.device_count() > 1:
             self.trainer.model = nn.DataParallel(self.trainer.model)
+            self.trainer.model(self.trainer.device)
         train = self._get_training_loader(
             folds=self.params["training_params"].get("train_folds"),
             name="train")
@@ -428,6 +430,7 @@ class IMDBEngine(Engine):
         LOGGER.info(f'Using {torch.cuda.device_count()} GPUs')
         if torch.cuda.device_count() > 1:
             self.trainer.model = nn.DataParallel(self.trainer.model)
+            self.trainer.model(self.trainer.device)
         train = self._get_training_loader(
             folds=self.params["training_params"].get("train_folds"),
             name="train")
